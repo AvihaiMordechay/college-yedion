0dee7c893697d88de2d14829ab73b5389d48679e
Avihai Mordechay <avihaimo1@gmail.com>
2024-08-22 12:03:42 +0300

    עדכון

--- Application/src/Components/MessagingSystem/MessagingTable.jsx
+++ Application/src/Components/MessagingSystem/MessagingTable.jsx
@@ -1,11 +1,17 @@
 import MainCard from "Components/Cards/MainCard";
 import * as React from "react";
 import { DataGrid } from "@mui/x-data-grid";
-
+import "Assets/Scss/mainLayout.scss";
 const MessagingTable = ({ rows, columns }) => {
   return (
     <MainCard>
-      <div style={{ height: 400, width: "100%", direction: "ltr" }}>
+      <div
+        style={{
+          height: 400,
+          width: "100%",
+          direction: "ltr",
+        }}
+      >
         <DataGrid
           rows={rows}
           columns={columns}
